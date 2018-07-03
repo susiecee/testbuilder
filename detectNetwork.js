ac85f284ca24ce66b57ac4ce4329ae7e3decb5b1
Suzie <suzie@fu.com>
2018-07-03 12:37:30 -0700

    Broken Step 3 - trying to add prefixCheck function to include arrays without ranges

--- detectNetwork.js
+++ detectNetwork.js
@@ -4,27 +4,40 @@ var detectNetwork = function(cardNumber) {
   
   let hasPrefix = function (prefix){
     return cardNumber.startsWith(prefix); 
+    //.startsWith takes in a string to use this function you need to convert numbers to strings
+    //prefixes does not need to be a string 
   }
+
 
   let prefixCheck = function (prefixArray , cardNumber) {
+    // debugger;
     for (let i = 0; i < prefixArray.length; i++) {
-  
-      let numToString =  prefixArray[i][0].toString(); 
-      let numToStringB = prefixArray[i][1].toString(); 
 
-      let cardNumPrefix = cardNumber.slice(0, numToString.length); //cardSlice
-      //NOTE: when comparing strings using unstrict equality, JS will convert strings to numbers automatically 
-      //For good practice, we are using parseInt to convert the string to # with let parseInt
-      let integerPrefix = parseInt(cardNumPrefix,10); //iCardSlice
-  
-      //we need to check for range between 622126 and 622925
-     if (integerPrefix >= prefixArray[i][0] && 
-     integerPrefix <= prefixArray[i][1]) { 
-        return true; 
+      // if there are ranges in the prefixArray 
+      if (prefixArray[i][0] && prefixArray[i][1]) {
+      
+          let numToString =  prefixArray[i][0].toString(); 
+          let numToStringB = prefixArray[i][1].toString(); 
+
+          let cardNumPrefix = cardNumber.slice(0, numToString.length); //cardSlice
+          //NOTE: when comparing strings using unstrict equality, JS will convert strings to numbers automatically 
+          //For good practice, we are using parseInt to convert the string to # with let parseInt
+          let integerPrefix = parseInt(cardNumPrefix,10); //iCardSlice
+      
+          //we need to check for range between 622126 and 622925
+        if (integerPrefix >= prefixArray[i][0] && 
+        integerPrefix <= prefixArray[i][1]) { 
+            return true; 
+          }
+        } else {
+      //if there are no ranges, check to see if prefix matches
+        if (hasPrefix(prefixArray[i])){
+          return true;
+        }
       }
     }
-    return false;
-  }
+  return false;
+}
   
   //==============================================================
   //check length of cardNumber - error handling for non-digits
@@ -41,31 +54,34 @@ var detectNetwork = function(cardNumber) {
     }
   //==============================================================
     
-    if ((count === 14) && 
+  if ((count === 14) && 
         (hasPrefix('38')  || hasPrefix('39'))) {
-      return ('Diner\'s Club');
+      return 'Diner\'s Club';
       } 
     if ((count === 15) && 
         (hasPrefix('34')  || hasPrefix('37'))) {
-      return ('American Express');
+      return 'American Express';
+      }
+    if ((count ===16 || count === 18 || count === 19) && 
+      prefixCheck([4903, 4905, 4911,4936,564182,633110,6333,6759], cardNumber)) { 
+        return 'Switch'; 
       }
     if ((count ===13 || count === 16 || count === 19) && 
         (hasPrefix('4'))) {
-      return ('Visa');
+      return 'Visa';
       }
     if ((count === 16) && 
         (hasPrefix('51')  || hasPrefix('52') || hasPrefix('53')
         || hasPrefix('54') || hasPrefix('55'))) {
-      return ('MasterCard');
+      return 'MasterCard';
       }
     if ((count === 16 || count === 19) && 
-        (hasPrefix('6011') || hasPrefix('65') || hasPrefix('644') || hasPrefix('645') 
-        || hasPrefix('646') || hasPrefix('647') || hasPrefix('648') || hasPrefix('649'))) {
-      return ('Discover');
-      }
+    prefixCheck(['6011', '65', '644','645','646','647','648','649'], cardNumber)) { 
+      return 'Discover'; 
+    }    
     if ((count >= 12 && count <=19) && 
          (hasPrefix('5018') || hasPrefix('5020') || hasPrefix('5038')|| hasPrefix('6304'))) {
-      return ('Maestro');
+      return 'Maestro';
       }
      if (count >=16 && count <=19) {
        let prefixArray =       
@@ -76,8 +92,7 @@ var detectNetwork = function(cardNumber) {
         ]; 
         
         if (prefixCheck(prefixArray, cardNumber) === true) {
-        return ('China UnionPay'); 
+        return 'China UnionPay'; 
         }
      }
-  }
-
+  } // function close
